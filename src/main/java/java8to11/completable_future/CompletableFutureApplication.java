a0deac86e716274d33c46e02bb557626fac4b378
Yonghwi Kim <yh0921k@gmail.com>
2023-02-27 09:20:29 +0900

    feat: Thread를 생성하는 세 번째 방법
    - 람다식 사용

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -3,13 +3,7 @@ package java8to11.completable_future;
 public class CompletableFutureApplication {
   public static void main(String[] args){
 
-    Thread thread = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        System.out.println("Thread : " + Thread.currentThread().getName());
-      }
-    });
-
+    Thread thread = new Thread(() -> System.out.println("Thread : " + Thread.currentThread().getName()));
     thread.start();
 
     System.out.println(Thread.currentThread().getName()); // main thread
