d15d6f7e3465eb50b3a7711f9c023033a71c3cc3
Yonghwi Kim <yh0921k@gmail.com>
2023-02-27 09:19:53 +0900

    feat: Thread를 생성하는 두 번째 방법
    - 익명 클래스 사용

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -3,18 +3,17 @@ package java8to11.completable_future;
 public class CompletableFutureApplication {
   public static void main(String[] args){
 
-    Thread myThread = new MyThread();
-    myThread.start();
+    Thread thread = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        System.out.println("Thread : " + Thread.currentThread().getName());
+      }
+    });
+
+    thread.start();
 
     System.out.println(Thread.currentThread().getName()); // main thread
 
-  }
-
-  static class MyThread extends Thread {
-    @Override
-    public void run() {
-      System.out.println("Thread : " + Thread.currentThread().getName());
-    }
   }
 }
 
