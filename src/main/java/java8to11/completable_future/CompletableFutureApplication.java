15eb5369a346a7fd5ebdd7cd36e66517b658b7b6
Yonghwi Kim <yh0921k@gmail.com>
2023-02-27 09:29:56 +0900

    feat: Thread - sleep()
    - 쓰레드가 슬립 상태인 동안 다른 쓰레드는 로직을 수행
    - 만일 다른 쓰레드에서 인터럽트가 발생하면 현재 로직상 catch()문이 수행됨

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -3,7 +3,14 @@ package java8to11.completable_future;
 public class CompletableFutureApplication {
   public static void main(String[] args){
 
-    Thread thread = new Thread(() -> System.out.println("Thread : " + Thread.currentThread().getName()));
+    Thread thread = new Thread(() -> {
+      try {
+        Thread.sleep(3000L);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println("Thread : " + Thread.currentThread().getName());
+    });
     thread.start();
 
     System.out.println(Thread.currentThread().getName()); // main thread
