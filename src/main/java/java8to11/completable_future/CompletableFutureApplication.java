a5fa84535d9d8bbddb97e4bfab61641c60e68887
Yonghwi Kim <yh0921k@gmail.com>
2023-02-27 09:55:25 +0900

    feat: Thread - join()
    - 다른 쓰레드 기다리기
    - 현재 메인 쓰레드가 다른 쓰레드를 기다리는 동안에 인터럽트를 당할 수 있으며, 점점 로직은 복잡해짐

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -4,21 +4,18 @@ public class CompletableFutureApplication {
   public static void main(String[] args) throws Exception {
 
     Thread thread = new Thread(() -> {
-      while (true) {
-        System.out.println("Thread : " + Thread.currentThread().getName());
-        try {
-          Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-          System.out.println("Exit");
-          return;
-        }
+      System.out.println("Thread : " + Thread.currentThread().getName());
+      try {
+        Thread.sleep(3000L);
+      } catch (InterruptedException e) {
+        throw new IllegalStateException(e);
       }
     });
     thread.start();
 
     System.out.println(Thread.currentThread().getName()); // main thread
-    Thread.sleep(3000L);
-    thread.interrupt();
+    thread.join();
+    System.out.println(thread + " is finished");
   }
 }
 
