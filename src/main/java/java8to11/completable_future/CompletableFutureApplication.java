ba373ac5f86b1c19e5894c36aa4f900a897b0979
Yonghwi Kim <yh0921k@gmail.com>
2023-02-28 12:35:00 +0900

    feat: ExecutorService - 작업 할당 및 종료

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -1,21 +1,14 @@
 package java8to11.completable_future;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CompletableFutureApplication {
   public static void main(String[] args) throws Exception {
+    ExecutorService executorService = Executors.newSingleThreadExecutor();
+    executorService.submit(() -> System.out.println("Thread : " + Thread.currentThread().getName()));
 
-    Thread thread = new Thread(() -> {
-      System.out.println("Thread : " + Thread.currentThread().getName());
-      try {
-        Thread.sleep(3000L);
-      } catch (InterruptedException e) {
-        throw new IllegalStateException(e);
-      }
-    });
-    thread.start();
-
-    System.out.println(Thread.currentThread().getName()); // main thread
-    thread.join();
-    System.out.println(thread + " is finished");
+    executorService.shutdown();
   }
 }
 
