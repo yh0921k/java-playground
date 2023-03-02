5635d8190e87dfcbad79da2396c9921d7e907cd3
Yonghwi Kim <yh0921k@gmail.com>
2023-03-02 23:50:21 +0900

    feat: CompletableFuture - 기본
    - 생성 및 작업완료

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -1,36 +1,15 @@
 package java8to11.completable_future;
 
-import java.util.Arrays;
-import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    CompletableFuture<String> java = new CompletableFuture<>();
+    java.complete("Java");
+    System.out.println("future.get() = " + java.get());
 
-    Callable<String> hello =
-        () -> {
-          Thread.sleep(2000L);
-          return "Hello";
-        };
-
-    Callable<String> java =
-        () -> {
-          Thread.sleep(3000L);
-          return "Java";
-        };
-
-    Callable<String> callable =
-        () -> {
-          Thread.sleep(1000L);
-          return "Callable";
-        };
-
-    String result = executorService.invokeAny(Arrays.asList(hello, java, callable));
-    System.out.println("result = " + result);
-
-    executorService.shutdown();
+    CompletableFuture<String> hello = CompletableFuture.completedFuture("Hello");
+    System.out.println("hello.get() = " + hello.get());
   }
 }
