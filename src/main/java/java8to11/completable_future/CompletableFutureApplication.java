4e8b05776ba568f9ad8fbc5fa40dcefe2238e038
Yonghwi Kim <yh0921k@gmail.com>
2023-03-03 01:09:52 +0900

    feat: CompletableFuture - thenCompose()
    - 두 작업이 서로 이어서 실행하도록 조합(두 작업이 연관관계가 있는 경우)

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -2,27 +2,25 @@ package java8to11.completable_future;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    ExecutorService executorService = Executors.newFixedThreadPool(4);
-
-    CompletableFuture<String> future =
+    CompletableFuture<String> hello =
         CompletableFuture.supplyAsync(
-                () -> {
-                  System.out.println("Hello : " + Thread.currentThread().getName());
-                  return "Hello";
-                },
-                executorService)
-            .thenApplyAsync(
-                (s) -> {
-                  System.out.println("Hello : " + Thread.currentThread().getName());
-                  return s.toUpperCase();
-                },
-                executorService);
+            () -> {
+              System.out.println("Hello : " + Thread.currentThread().getName());
+              return "Hello";
+            });
 
+    CompletableFuture<String> future = hello.thenCompose(CompletableFutureApplication::getMessage);
     System.out.println("future.get() = " + future.get());
+  }
+
+  private static CompletableFuture<String> getMessage(String message) {
+    return CompletableFuture.supplyAsync(
+        () -> {
+          System.out.println("World : " + Thread.currentThread().getName());
+          return message + "World";
+        });
   }
 }
