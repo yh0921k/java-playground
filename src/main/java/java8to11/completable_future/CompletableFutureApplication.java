08e9ab036b493677ec61075e058878ffa05f4953
Yonghwi Kim <yh0921k@gmail.com>
2023-03-02 23:54:06 +0900

    feat: CompletableFuture - 반환값이 있는 경우

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -5,8 +5,11 @@ import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+    CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
       System.out.println("Hello : " + Thread.currentThread().getName());
+      return "Hello";
     });
+
+    System.out.println("future.get() = " + future.get());
   }
 }
