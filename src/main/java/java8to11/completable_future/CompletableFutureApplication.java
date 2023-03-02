4baa5a109c891931ded88117e8df81ab62eb95c6
Yonghwi Kim <yh0921k@gmail.com>
2023-03-03 01:13:08 +0900

    feat: CompletableFuture - exceptionally(Function)
    - 예외처리

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -2,25 +2,21 @@ package java8to11.completable_future;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
+    boolean throwError = true;
+
     CompletableFuture<String> hello =
         CompletableFuture.supplyAsync(
-            () -> {
-              System.out.println("Hello : " + Thread.currentThread().getName());
-              return "Hello";
-            });
+                () -> {
+                  if (throwError) {
+                    throw new IllegalArgumentException();
+                  }
+                  System.out.println("Hello : " + Thread.currentThread().getName());
+                  return "Hello";
+                })
+            .exceptionally(ex -> "Error!");
 
-    CompletableFuture<String> world =
-        CompletableFuture.supplyAsync(
-            () -> {
-              System.out.println("World : " + Thread.currentThread().getName());
-              return "World";
-            });
-
-    CompletableFuture<Void> future =
-        CompletableFuture.anyOf(hello, world).thenAccept((s) -> System.out.println("s = " + s));
-    System.out.println("future.get() = " + future.get());
+    System.out.println("hello.get() = " + hello.get());
   }
 }
