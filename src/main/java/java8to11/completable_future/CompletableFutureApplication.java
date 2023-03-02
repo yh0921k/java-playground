d20270ba09994e9549279fbf36d6bbce7f873ce7
Yonghwi Kim <yh0921k@gmail.com>
2023-03-03 00:07:29 +0900

    feat: CompletableFuture - 원하는 Executor(쓰레드풀)를 사용하는 경우
    - 동일 쓰레드에서 수행되는 결과 확인

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -2,15 +2,20 @@ package java8to11.completable_future;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(4);
+
     CompletableFuture<Void> future =
         CompletableFuture.supplyAsync(
                 () -> {
                   System.out.println("Hello : " + Thread.currentThread().getName());
                   return "Hello";
-                })
+                },
+                executorService)
             .thenRun(
                 () -> {
                   System.out.println("Hello : " + Thread.currentThread().getName());
