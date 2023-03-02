baf99e530ad35708c327579450102ed720cec697
Yonghwi Kim <yh0921k@gmail.com>
2023-03-02 23:11:30 +0900

    feat: Callable & Future - 작업 취소하기
    - 파라미터로 true를 전달하면 현재 진행중인 쓰레드를 interrupt하고 그렇지 않으면 현재 진행중인 작업이 끝날때까지 기다림

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -6,22 +6,21 @@ public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
     ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-    Callable<String> task = () -> {
-      Thread.sleep(2000L);
-      return "Hello";
-    };
+    Callable<String> task =
+        () -> {
+          Thread.sleep(2000L);
+          return "Hello";
+        };
 
     Future<String> future = executorService.submit(task);
     System.out.println(future.isDone());
     System.out.println("Started");
 
-    String result = future.get();
+    future.cancel(false);
 
     System.out.println(future.isDone());
-    System.out.println("result = " + result);
     System.out.println("End");
 
     executorService.shutdown();
   }
 }
-
