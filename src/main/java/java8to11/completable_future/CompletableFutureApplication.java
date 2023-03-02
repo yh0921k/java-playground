7b4b2cbd3a488ad9c3863c6fc22ae388d1fbd1ee
Yonghwi Kim <yh0921k@gmail.com>
2023-03-02 23:21:07 +0900

    feat: Callable & Future - 여러 작업 동시 실행하기
    - 작업을 할당하는 쓰레드 수에 따라 결과가 달라짐에 유의

--- src/main/java/java8to11/completable_future/CompletableFutureApplication.java
+++ src/main/java/java8to11/completable_future/CompletableFutureApplication.java
@@ -1,25 +1,35 @@
 package java8to11.completable_future;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class CompletableFutureApplication {
   public static void main(String[] args) throws ExecutionException, InterruptedException {
-    ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-    Callable<String> task =
+    Callable<String> hello =
         () -> {
           Thread.sleep(2000L);
           return "Hello";
         };
 
-    Future<String> future = executorService.submit(task);
-    System.out.println(future.isDone());
-    System.out.println("Started");
+    Callable<String> java =
+        () -> {
+          Thread.sleep(3000L);
+          return "Java";
+        };
 
-    future.cancel(false);
+    Callable<String> callable =
+        () -> {
+          Thread.sleep(1000L);
+          return "Callable";
+        };
 
-    System.out.println(future.isDone());
-    System.out.println("End");
+    List<Future<String>> futures = executorService.invokeAll(Arrays.asList(hello, java, callable));
+    for (Future<String> future : futures) {
+      System.out.println("future.get() = " + future.get());
+    }
 
     executorService.shutdown();
   }
